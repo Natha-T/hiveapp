3d25201ec269b74317d0b1800bc43afe4473a6c7
Nicolas Wagner <contact@wagner-nicolas.com>
2023-04-13 13:34:27 +0200

    feat: init next13 ts

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,16 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "/Users/n1c0/dev/goodhive-app/app/layout.tsx": [
+      "/Users/n1c0/dev/goodhive-app/app/globals.css"
+    ],
+    "/Users/n1c0/dev/goodhive-app/app/page.tsx": [
+      "/Users/n1c0/dev/goodhive-app/node_modules/next/font/google/target.css?{\"path\":\"app/page.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ]
+  },
+  "cssModules": {
+    "/Users/n1c0/dev/goodhive-app/app/page": [
+      "/Users/n1c0/dev/goodhive-app/node_modules/next/font/google/target.css?{\"path\":\"app/page.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
+      "/Users/n1c0/dev/goodhive-app/app/globals.css"
+    ]
+  }
+}
