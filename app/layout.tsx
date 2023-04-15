89297d3c56ccbf499bffe44a0c7f2a11b2b9f9bf
Nicolas Wagner <contact@wagner-nicolas.com>
2023-04-15 13:39:50 +0200

    feat: add menu (WIP)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import "./globals.css";
 
+import { NavBar } from "./components/nav-bar";
+
 export const metadata = {
   title: "GoodHive",
   description: "Freelancing for the future",
@@ -12,7 +14,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <NavBar />
+        {children}
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,10 @@
-import Image from "next/image";
-
 import { Inter } from "next/font/google";
-
-import { NavBar } from "./components/nav-bar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
-      <NavBar />
       <main className="mx-5">
         <h1 className="mt-5 text-2xl">Home Page</h1>
         <p className="my-10 text-lg">
