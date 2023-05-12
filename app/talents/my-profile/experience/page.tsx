fe6fe47b0d16570d024f1974a00504e5f2414321
LittleSweetGun <jeanne.zak63@gmail.com>
2023-05-12 16:45:07 +0200

    feat: add autosuggest for skills

--- app/talents/my-profile/experience/page.tsx
+++ app/talents/my-profile/experience/page.tsx
@@ -10,10 +10,9 @@ import toast from "react-hot-toast";
 import { Button } from "../../../components/button";
 
 export default function CreateJob() {
-  const invoiceInputValue = useRef(null);
   const [isLoading, setIsLoading] = useState(false);
-  const [isRenderedPage, setIsRenderedPage] = useState<boolean>(true);
   const [value, setValue] = useState("");
+  const [selectedSkills, setSelectedSkills] = useState<string[]>([]);
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -29,16 +28,19 @@ export default function CreateJob() {
       start: formData.get("start"),
       end: formData.get("end"),
       description: formData.get("description"),
-      skills: formData.get("skills"),
+      skills: selectedSkills,
     };
-    // TODO: POST formData to the server with fetch
-    const experienceResponse = await fetch("/api/companies/", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(dataForm),
-    });
+
+    const experienceResponse = await fetch(
+      "/api/talents/my-profile/experience",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(dataForm),
+      }
+    );
 
     const experienceData = await experienceResponse.json();
 
@@ -51,6 +53,86 @@ export default function CreateJob() {
     }
   };
 
+  //TODO: Put the following code in a Autosuggest Input component
+  const languages = [
+    "JavaScript",
+    "Solidity",
+    "SQL",
+    "C#",
+    "Tailwind",
+    "EtherJS",
+    "TypeScript",
+    "IPFS",
+    "ReactJS",
+    "NextJS",
+    // Add more skills here if needed...
+  ];
+
+  const AutosuggestInput = () => {
+    const [value, setValue] = useState("");
+    const [suggestions, setSuggestions] = useState<string[]>([]);
+
+    const getSuggestions = (value: string) => {
+      const inputValue = value.trim().toLowerCase();
+      const inputLength = inputValue.length;
+
+      return inputLength === 0
+        ? []
+        : languages.filter(
+            (skill) => skill.toLowerCase().slice(0, inputLength) === inputValue
+          );
+    };
+
+    const onSuggestionsFetchRequested = ({ value }: any) => {
+      setSuggestions(getSuggestions(value));
+    };
+
+    const onSuggestionsClearRequested = () => {
+      setSuggestions([]);
+    };
+
+    const onSuggestionSelected = (
+      event: React.FormEvent<HTMLInputElement>,
+      { suggestion }: Autosuggest.SuggestionSelectedEventData<any>
+    ) => {
+      if (!selectedSkills.includes(suggestion))
+        setSelectedSkills([...selectedSkills, suggestion]);
+      setValue("");
+    };
+    const renderSuggestion = (suggestion: string) => (
+      <div className="mx-4 hover:text-[#FFC905] ">{suggestion}</div>
+    );
+
+    const inputProps = {
+      placeholder: "JavaScript, NextJS,...",
+      type: "text",
+      maxLength: 255,
+      name: "skills",
+      value,
+      onChange: (
+        event: React.FormEvent<HTMLElement>,
+        { newValue }: { newValue: string }
+      ) => {
+        setValue(newValue);
+      },
+      className:
+        "ml-3 ml-1 pl-1 border-b border-gray-600 block me-5 w-full px-4 py-2 text-base font-normal text-gray-600 bg-clip-padding transition ease-in-out focus:text-black focus:outline-none focus:ring-0",
+    };
+
+    return (
+      <div className="relative">
+        <Autosuggest
+          suggestions={getSuggestions(value)}
+          onSuggestionsFetchRequested={onSuggestionsFetchRequested}
+          onSuggestionsClearRequested={onSuggestionsClearRequested}
+          getSuggestionValue={(skill) => skill}
+          onSuggestionSelected={onSuggestionSelected}
+          renderSuggestion={renderSuggestion}
+          inputProps={inputProps}
+        />
+      </div>
+    );
+  };
   return (
     <main className="mx-5">
       <h1 className="my-5 text-2xl border-b-[1px] border-slate-300">
@@ -68,7 +150,7 @@ export default function CreateJob() {
                   Title*
                 </label>
                 <input
-                  className="form-control block w-full px-4 py-2 text-base font-normal text-gray-600 bg-white bg-clip-padding border border-solid border-[#FFC905] rounded-lg hover:shadow-lg transition ease-in-out m-0 focus:text-black focus:bg-white focus:border-[#FF8C05] focus:outline-none"
+                  className="form-control block w-full px-4 py-2 text-base font-normal text-gray-600 bg-gray-100 rounded-full bg-clip-padding border border-solid border-[#FFC905]  hover:shadow-lg transition ease-in-out m-0 focus:text-black focus:bg-white focus:border-[#FF8C05] focus:outline-none"
                   placeholder="Try Developer Solidity, Rust, C++..."
                   name="title"
                   type="text"
@@ -84,7 +166,7 @@ export default function CreateJob() {
                   Employment Type*
                 </label>
                 <input
-                  className="form-control block w-full px-4 py-2 text-base font-normal text-gray-600 bg-white bg-clip-padding border border-solid border-[#FFC905] rounded-lg hover:shadow-lg transition ease-in-out m-0 focus:text-black focus:bg-white focus:border-[#FF8C05] focus:outline-none"
+                  className="form-control block w-full px-4 py-2 text-base font-normal text-gray-600 bg-gray-100 rounded-full bg-clip-padding border border-solid border-[#FFC905] hover:shadow-lg transition ease-in-out m-0 focus:text-black focus:bg-white focus:border-[#FF8C05] focus:outline-none"
                   placeholder="Please select"
                   name="type-employment"
                   type="text"
@@ -94,17 +176,16 @@ export default function CreateJob() {
                 />
               </div>
             </div>
-
-            <div className="flex flex-col gap-4 mt-4 sm:flex-row">
+            <div className="flex flex-col gap-4 mt-6 sm:flex-row">
               <div className="flex-1">
                 <label
                   htmlFor="designation"
-                  className="inline-block ml-3 text-base text-black form-label mt-4"
+                  className="inline-block ml-3 text-base text-black form-label"
                 >
                   Company Name*
                 </label>
                 <input
-                  className="form-control block w-full px-4 py-2 text-base font-normal text-gray-600 bg-white bg-clip-padding border border-solid border-[#FFC905] rounded-lg hover:shadow-lg transition ease-in-out m-0 focus:text-black focus:bg-white focus:border-[#FF8C05] focus:outline-none"
+                  className="form-control block w-full px-4 py-2 text-base font-normal text-gray-600 bg-gray-100 rounded-full bg-clip-padding border border-solid border-[#FFC905] hover:shadow-lg transition ease-in-out m-0 focus:text-black focus:bg-white focus:border-[#FF8C05] focus:outline-none"
                   placeholder="Try Developer Solidity, Rust, C++..."
                   name="title"
                   type="text"
@@ -120,7 +201,7 @@ export default function CreateJob() {
                   Location*
                 </label>
                 <input
-                  className="form-control block w-full px-4 py-2 text-base font-normal text-gray-600 bg-white bg-clip-padding border border-solid border-[#FFC905] rounded-lg hover:shadow-lg transition ease-in-out m-0 focus:text-black focus:bg-white focus:border-[#FF8C05] focus:outline-none"
+                  className="form-control block w-full px-4 py-2 text-base font-normal text-gray-600 bg-gray-100 rounded-full bg-clip-padding border border-solid border-[#FFC905] hover:shadow-lg transition ease-in-out m-0 focus:text-black focus:bg-white focus:border-[#FF8C05] focus:outline-none"
                   type="text"
                   name="location"
                   placeholder="Paris, London, Remote..."
@@ -129,8 +210,7 @@ export default function CreateJob() {
                 />
               </div>
             </div>
-
-            <div className="flex flex-col gap-4 mt-10 sm:flex-row">
+            <div className="flex flex-col gap-4 mt-6 sm:flex-row">
               <div className="flex-1">
                 <label
                   htmlFor="start"
@@ -139,7 +219,7 @@ export default function CreateJob() {
                   Start Date*
                 </label>
                 <input
-                  className="form-control block me-5 w-full px-4 py-2 text-base font-normal text-gray-600 bg-white bg-clip-padding border border-solid border-[#FFC905] rounded-lg hover:shadow-lg transition ease-in-out m-0 focus:text-black focus:bg-white focus:border-[#FF8C05] focus:outline-none"
+                  className="form-control block me-5 w-full px-4 py-2 text-base font-normal text-gray-600 bg-gray-100 rounded-full bg-clip-padding border border-solid border-[#FFC905]  hover:shadow-lg transition ease-in-out m-0 focus:text-black focus:bg-white focus:border-[#FF8C05] focus:outline-none"
                   type="number"
                   name="start"
                   maxLength={100}
@@ -153,7 +233,7 @@ export default function CreateJob() {
                   End Date*
                 </label>
                 <input
-                  className="form-control block me-5 w-full px-4 py-2 text-base font-normal text-gray-600 bg-white bg-clip-padding border border-solid border-[#FFC905] rounded-lg hover:shadow-lg transition ease-in-out m-0 focus:text-black focus:bg-white focus:border-[#FF8C05] focus:outline-none"
+                  className="form-control block me-5 w-full px-4 py-2 text-base font-normal text-gray-600 bg-gray-100 rounded-full bg-clip-padding border border-solid border-[#FFC905] hover:shadow-lg transition ease-in-out m-0 focus:text-black focus:bg-white focus:border-[#FF8C05] focus:outline-none"
                   type="number"
                   name="end"
                   maxLength={100}
@@ -162,34 +242,55 @@ export default function CreateJob() {
             </div>
             <div>
               <label
-                htmlFor="designation"
-                className="inline-block ml-3 text-base text-black form-label mt-4"
+                htmlFor="description"
+                className="inline-block ml-3 text-base text-black form-label mt-6"
               ></label>
             </div>
             <div>
               <textarea
-                name="designation"
-                className="form-control block w-full px-4 py-2 text-base font-normal text-gray-600 bg-white bg-clip-padding border border-solid border-[#FFC905] rounded-lg hover:shadow-lg transition ease-in-out m-0 focus:text-black focus:bg-white focus:border-[#FF8C05] focus:outline-none"
+                name="description"
+                className="form-control block w-full px-4 py-2 text-base font-normal text-gray-600 bg-gray-100 bg-clip-padding border border-solid border-[#FFC905] rounded-lg hover:shadow-lg transition ease-in-out m-0 focus:text-black focus:bg-white focus:border-[#FF8C05] focus:outline-none"
                 placeholder="Description of the past assignement / projects"
                 maxLength={255}
-                rows={5}
+                rows={3}
               />
             </div>
-            <div className="flex-1">
-              <label
-                htmlFor="skills"
-                className="inline-block ml-3 text-base text-black form-label"
-              >
-                Skills
-              </label>
-              <input
-                className="form-control block w-full px-4 py-2 text-base font-normal text-gray-600 bg-white bg-clip-padding border border-solid border-[#FFC905] rounded-lg hover:shadow-lg transition ease-in-out m-0 focus:text-black focus:bg-white focus:border-[#FF8C05] focus:outline-none"
-                type="text"
-                name="skills"
-                placeholder="JavaScript, NextJS..."
-                required
-                maxLength={100}
-              />
+            <div className="flex flex-col gap-4 mt-10 sm:flex-row">
+              <div className="flex-1">
+                <label
+                  htmlFor="skills"
+                  className="inline-block font-bold ml-3 text-base text-black form-label"
+                >
+                  Skills
+                </label>
+                <div className="absolute pr-10 pt-1 form-control w-full text-base font-normal text-gray-600 bg-white ">
+                  <AutosuggestInput />
+                </div>
+                <div className="pt-10">
+                  {selectedSkills.length > 0 && (
+                    <div className="flex flex-wrap mt-4 ">
+                      {selectedSkills.map((skill, index) => (
+                        <div
+                          key={index}
+                          className="border border-[#FFC905] flex items-center bg-gray-200 rounded-full py-1 px-3 m-1"
+                        >
+                          <span className="mr-2">{skill}</span>
+                          <button
+                            onClick={() =>
+                              setSelectedSkills(
+                                selectedSkills.filter((_, i) => i !== index)
+                              )
+                            }
+                            className="text-black bg-gray-400 rounded-full w-6 focus:outline-none"
+                          >
+                            &#10005;
+                          </button>
+                        </div>
+                      ))}
+                    </div>
+                  )}
+                </div>
+              </div>
             </div>
             <div className="mt-10 text-right">
               {isLoading ? (
