c6f06319035523c735be71e1b48abb9736db5e57
LittleSweetGun <jeanne.zak63@gmail.com>
2023-05-30 15:58:08 +0200

    feat: connect job-search to DB

---
+++ app/api/talents/job-search/route.ts
@@ -0,0 +1,30 @@
+import postgres from "postgres";
+
+const sql = postgres(process.env.DATABASE_URL || "", {
+  ssl: {
+    rejectUnauthorized: false, // This allows connecting to a database with a self-signed certificate
+  },
+});
+
+export async function GET(request: Request) {
+  {
+    try {
+      const talents = await sql`SELECT * FROM goodhive.job_offers`;
+      const formattedTalents = talents.map((item) => ({
+        title: item.title,
+        typeEngagement: item.type_engagement,
+        description: item.description,
+        duration: item.duration,
+        rate: item.rate_per_hour,
+        budget: item.budget,
+        skills: item.skills.split(","),
+      }));
+      return new Response(JSON.stringify(formattedTalents)); //
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      return new Response(JSON.stringify({ message: "Error fetching data" }), {
+        status: 500,
+      });
+    }
+  }
+}
