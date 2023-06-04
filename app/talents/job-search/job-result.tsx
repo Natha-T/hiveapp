3382ca492ff8cde871b03b9704566cec42701b0a
Nicolas Wagner <contact@wagner-nicolas.com>
2023-06-04 15:53:34 +0200

    fix: force build

--- app/talents/job-search/job-result.tsx
+++ app/talents/job-search/job-result.tsx
@@ -1,30 +1,11 @@
 "use client";
 
-import { useState } from "react";
-
-import Modal from "../../components/modal";
 import { Card } from "../../components/card";
 
-//ADD REAL DATA TO MAP THE CARD COMPONENT
-
+//TODO: ADD REAL DATA TO MAP THE CARD COMPONENT
 export default function JobResult() {
-  const [isOpen, setIsOpen] = useState(false);
-
   return (
     <>
-      <button onClick={() => setIsOpen(true)}>Open Modal</button>
-      <Modal onClose={() => setIsOpen(false)} open={isOpen}>
-        <p>
-          I'm a modal window, I don't use portals but use the dialog element
-          from the platform.
-        </p>
-        <p>
-          Also tabbing is locked inside me go ahead and try tabbing to the
-          button behind me.
-        </p>
-        <button onClick={() => setIsOpen(false)}>Close</button>
-      </Modal>
-
       <div className="grid min-w-full grid-flow-row grid-cols-1 gap-3 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-3 3xl:grid-cols-44xl:grid-cols-3">
         <Card
           type="company"
