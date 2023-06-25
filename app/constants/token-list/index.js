ff4ee14678cbac6a8a9127fd854943106e9979cc
Nicolas Wagner <contact@wagner-nicolas.com>
2023-06-25 17:04:00 +0200

    feat: add tokens list

---
+++ app/constants/token-list/index.js
@@ -0,0 +1,3 @@
+export { ethereumTokens } from "./ethereum"; // https://github.com/viaprotocol/tokenlists/blob/main/tokenlists/ethereum.json
+export { polygonTokens } from "./polygon"; // from https://github.com/maticnetwork/polygon-token-list/blob/dev/src/tokens/polygonTokens.json
+export { gnosisChainTokens } from "./gnosis-chain"; // from https://github.com/viaprotocol/tokenlists/blob/main/tokenlists/gnosis.json
