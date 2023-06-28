90171a80cc6aeb9f7534e029ef64eaa6f09af7b4
LittleSweetGun <jeanne.zak63@gmail.com>
2023-06-28 16:51:28 +0200

    refactor: corrected gnosis chain value

--- app/constants/chains.js
+++ app/constants/chains.js
@@ -1,5 +1,5 @@
 export const chains = [
   { value: "ethereum", label: "Ethereum" },
   { value: "polygon", label: "Polygon" },
-  { value: "gnosis", label: "Gnosis Chain" },
+  { value: "gnosis-chain", label: "Gnosis Chain" },
 ];
