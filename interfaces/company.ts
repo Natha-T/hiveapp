ca46b792a283b455ba8e22c82f78bf7213f35358
Natha-T <lenaat220@gmail.com>
2023-07-05 23:32:58 +0200

    feat: display job by company

---
+++ interfaces/company.ts
@@ -0,0 +1,16 @@
+interface Company {
+  headline: string;
+  designation: string;
+  address: string;
+  country: string;
+  city: string;
+  phoneCountryCode: string;
+  phoneNumber: string;
+  email: string;
+  telegram: string;
+  details: string;
+  imageUrl: string;
+  wallet_address: string;
+}
+
+export default Company;
